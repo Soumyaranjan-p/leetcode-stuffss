2c760fcb5b9be870a15a0b2d651d13ab11ef7705
Soumya-ranjan <147945310+Soumyaranjan-p@users.noreply.github.com>
2026-02-01 18:28:52 +0530

    Time: 0 ms (100%), Space: 7.7 MB (86.31%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if (num < 2) return true;
+        
+        long low = 0, high = num;  // Use long to prevent overflow
+        
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+            long square = mid * mid;
+            
+            if (square == num) {
+                return true;
+            } else if (square < num) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return false;
+    }
+};
